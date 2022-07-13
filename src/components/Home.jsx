8dd8fd8b1a1760e85338053b942dad7c0bdaf651
SungHo826 <sungho.park.826@gmail.com>
2022-07-13 11:46:57 -0700

    regular commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const BodyStyled = styled.div`
+  // ::-webkit-scrollbar {
+  //   display: none; /* Chrome, Safari */
+  // }
+
+  // /* small desktop horizontal */
+  // @media only screen and (max-width: 1100px) {
+  //   width: 100vw;
+  // }
+`;
+
+function BasicCom() {
+  return <BodyStyled></BodyStyled>;
+}
+export default BasicCom;
